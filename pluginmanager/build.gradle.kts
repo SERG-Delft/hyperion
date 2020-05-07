ed6e546d4c4bc5ca0678136ec8d7ba0238b236ed
David Laurentius Arie Moolenaar <david@modare.nl>
2020-05-07 10:08:22 +0200

    Add pluginmanager folder

---
+++ pluginmanager/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    application
+    jacoco
+    kotlin("jvm")
+    id("org.jetbrains.intellij") version "0.4.18"
+    id("io.gitlab.arturbosch.detekt").version("1.8.0")
+    id("com.github.johnrengelman.shadow").version("5.2.0")
+}
+
+application {
+    mainClassName = "nl.tudelft.hyperion.pluginmanager.Main"
+}
+
+dependencies {
+    implementation("io.lettuce:lettuce-core:5.3.0.RELEASE")
+}
+
+jacoco {
+    toolVersion = "0.8.5"
+    reportsDir = file("$buildDir/jacoco")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = false
+        html.destination = file("${buildDir}/jacocoHtml")
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                counter = "BRANCH"
+                minimum = "0.8".toBigDecimal()
+            }
+
+            limit {
+                counter = "LINE"
+                minimum = "0.8".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
+tasks.build {
+    dependsOn(tasks.jacocoTestReport)
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+    destinationDir = File("./");
+}
