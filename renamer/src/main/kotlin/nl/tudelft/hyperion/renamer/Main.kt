3c4701feea4447426f41fee899281031bb292642
Ynze ter Horst <ysbrandth@outlook.com>
2020-05-08 12:01:15 +0200

    Extract main arguments

--- renamer/src/main/kotlin/nl/tudelft/hyperion/renamer/Main.kt
+++ renamer/src/main/kotlin/nl/tudelft/hyperion/renamer/Main.kt
@@ -6,5 +6,6 @@ import java.nio.file.Path
 
 
 fun main(vararg args: String) {
-    val instance = RenamePlugin(Configuration.load(Path.of("./renamer/config.yaml").toAbsolutePath()))
+    val instance = RenamePlugin(Configuration.load(Path.of(args.get(0)).toAbsolutePath()))
+    Thread.sleep(Long.MAX_VALUE)
 }
