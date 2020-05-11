7f65ee0da6568463c951e5cac1be151bdf815370
Thijs Molendijk <info@thijsmolendijk.nl>
2020-05-11 15:05:03 +0200

    feat(pipeline): Completely untested first async implementation of pipeline plugin

--- pipeline/common/build.gradle.kts
+++ pipeline/common/build.gradle.kts
@@ -10,6 +10,22 @@ jacoco {
     reportsDir = file("$buildDir/jacoco")
 }
 
+dependencies {
+    // Yaml/JSON deserialization
+    implementation("com.fasterxml.jackson.core", "jackson-databind", "2.9.4")
+    implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", "2.9.4")
+    implementation("com.fasterxml.jackson.datatype", "jackson-datatype-joda", "2.9.4")
+    implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", "2.9.4")
+
+    // Logging
+    implementation("io.github.microutils", "kotlin-logging", "1.7.9")
+    implementation("org.slf4j", "slf4j-simple", "1.7.28")
+
+    // Redis
+    implementation("io.lettuce", "lettuce-core", "5.3.0.RELEASE")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8", "1.3.5")
+}
+
 tasks.jacocoTestReport {
     reports {
         xml.isEnabled = false
