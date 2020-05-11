5e931bf81d8278b4282e2adbcfe0b8a984f1468d
David Laurentius Arie Moolenaar <david@modare.nl>
2020-05-11 11:13:18 +0200

    Update sampleplugin to use logging

--- sampleplugin/src/main/kotlin/nl/tudelft/hyperion/sampleplugin/Main.kt
+++ sampleplugin/src/main/kotlin/nl/tudelft/hyperion/sampleplugin/Main.kt
@@ -6,13 +6,16 @@ import nl.tudelft.hyperion.pluginmanager.hyperionplugin.SamplePlugin
 import nl.tudelft.hyperion.pluginmanager.hyperionplugin.PluginConfiguration
 import java.nio.file.Path
 
+private val logger = mu.KotlinLogging.logger {}
+
 fun main(vararg args: String) {
-    println("Loading config from ${args[0]}")
+
+    logger.info{"Loading config from ${args[0]}"}
     val config = PluginConfiguration.load(Path.of(args[0]).toAbsolutePath())
-    println("Starting plugin ${config.name}")
+    logger.info{"Starting plugin ${config.name}"}
     SamplePlugin(config)
 
-    println("Going to sleep in ${Thread.currentThread().name}")
+    logger.info{"Going to sleep in ${Thread.currentThread().name}"}
 
     while (true) {
         Thread.sleep(Long.MAX_VALUE)
