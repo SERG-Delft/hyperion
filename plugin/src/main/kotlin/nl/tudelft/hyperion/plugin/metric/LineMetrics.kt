0895dc386015cb0939546c7c03da65e83bfee31b
BlackQuests <maartenlips2@gmail.com>
2020-05-12 11:27:43 +0200

    Add automatic period formatter for Metric intervals

--- plugin/src/main/kotlin/nl/tudelft/hyperion/plugin/metric/LineMetrics.kt
+++ plugin/src/main/kotlin/nl/tudelft/hyperion/plugin/metric/LineMetrics.kt
@@ -1,5 +1,8 @@
 package nl.tudelft.hyperion.plugin.metric
 
+import org.joda.time.Period
+import org.joda.time.format.PeriodFormatterBuilder
+
 /**
  * Represents all metrics for a given line.
  */
@@ -8,5 +11,18 @@ class LineMetrics(val metrics: List<IntervalMetric>) {
     data class IntervalMetric(
             val interval: Int,
             val metric: Metric
-    )
+    ) {
+        private val formatter = PeriodFormatterBuilder()
+                .appendMonths().appendSuffix("M")
+                .appendWeeks().appendSuffix("w")
+                .appendDays().appendSuffix("d")
+                .appendHours().appendSuffix("h")
+                .appendMinutes().appendSuffix("min")
+                .appendSeconds().appendSuffix("s")
+                .toFormatter()
+
+        fun getIntervalFormat(): String {
+            return formatter.print(Period(interval * 1000L).normalizedStandard())
+        }
+    }
 }
