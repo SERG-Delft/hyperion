7b53f494bffbaf19e509c4654b06911fd5aa8ea2
Ynze ter Horst <ysbrandth@outlook.com>
2020-05-18 11:58:18 +0200

    Remove redis dependencies

--- pipeline/plugins/renamer/build.gradle.kts
+++ pipeline/plugins/renamer/build.gradle.kts
@@ -10,9 +10,6 @@ plugins {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.70")
 
-    implementation("redis.clients", "jedis", "3.2.0")
-    implementation("io.lettuce", "lettuce-core", "5.3.0.RELEASE")
-
     testImplementation("io.mockk", "mockk", "1.10.0")
 
     implementation(project(":pipeline:common"))
