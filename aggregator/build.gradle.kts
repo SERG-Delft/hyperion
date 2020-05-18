2e8b223fbfdc3f60e6476c2ab469dfd0b2bd8fbf
Ynze ter Horst <ysbrandth@outlook.com>
2020-05-18 14:25:07 +0200

    Remove and add modules for merge with master

---
+++ aggregator/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    application
+    jacoco
+    kotlin("jvm")
+    id("io.gitlab.arturbosch.detekt").version("1.8.0")
+    id("com.github.johnrengelman.shadow").version("5.2.0")
+}
+
+application {
+    mainClassName = "nl.tudelft.hyperion.aggregator.Main"
+}
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.8.0")
+}
+
+jacoco {
+    toolVersion = "0.8.5"
+    reportsDir = file("$buildDir/jacoco")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = false
+        html.destination = file("${buildDir}/jacocoHtml")
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                counter = "BRANCH"
+                minimum = "0.8".toBigDecimal()
+            }
+
+            limit {
+                counter = "LINE"
+                minimum = "0.8".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
+tasks.build {
+    dependsOn(tasks.jacocoTestReport)
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+    destinationDir = File("./");
+}
---
+++ plugin/src/main/kotlin/nl/tudelft/hyperion/plugin/Main.kt
@@ -0,0 +1,11 @@
+@file:JvmName("Main")
+
+package nl.tudelft.hyperion.plugin
+
+fun main(vararg args: String) {
+    println("Plugin shows itself UwU")
+}
+
+fun add(x: Int, y: Int): Int {
+    return x + y
+}
