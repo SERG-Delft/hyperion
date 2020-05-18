e5ff7731ed3be4c3b50a81c8e4c42e3185210f30
Ynze ter Horst <ysbrandth@outlook.com>
2020-05-18 13:33:31 +0200

    Update tests to match new configuration

--- pipeline/plugins/extractor/src/test/kotlin/nl/tudelft/hyperion/pipeline/extractor/configuration/ConfigurationTests.kt
+++ pipeline/plugins/extractor/src/test/kotlin/nl/tudelft/hyperion/pipeline/extractor/configuration/ConfigurationTests.kt
@@ -1,95 +1,51 @@
 package nl.tudelft.hyperion.pipeline.extractor.configuration
 
 import nl.tudelft.hyperion.pipeline.extractor.Configuration
-import nl.tudelft.hyperion.pipeline.PipelineRedisConfiguration
+import nl.tudelft.hyperion.pipeline.readYAMLConfig
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions
+import java.io.File
+import java.nio.file.Files
 
 class ConfigurationTests {
+    private val yaml = """
+        plugin :
+          pluginManager: "1.2.3.4:4567"
+          id : "plugin1"
+        
+        field : "message"
+        match : "\\[.+?\\] INFO [^:]+:(\\d+) (-) .+"
+        extract :
+          - to : "location.line"
+            type : "number"
+          - to : "dash"
+            type : "string"
+    """.trimIndent()
+
     @Test
-    fun parseRedisConfiguration() {
-        val yaml = """
-            name: "Plugin1"
-            redis:
-              host: "192.168.2.168"
-              port: 6379
+    fun parsePluginConfiguration() {
+        val config = parseConfig(yaml)
 
-            field : "message"
-            match : "\\[.+?\\] INFO [^:]+:(\\d+) (-) .+"
-            extract :
-              - to : "location.line"
-                type : "number"
-              - to : "dash"
-                type : "string"
-        """.trimIndent()
-
-        val config = Configuration.parse(yaml)
-
-        // Redis Configuration
-        Assertions.assertEquals("Plugin1", config.name)
-        Assertions.assertEquals("192.168.2.168", config.redis.host)
-        Assertions.assertEquals(6379, config.redis.port)
+        Assertions.assertEquals("plugin1", config.plugin.id)
+        Assertions.assertEquals("1.2.3.4:4567", config.plugin.pluginManager)
     }
 
     @Test
-    fun parseExtractionConfiguration() {
-        val yaml = """
-            name: "Plugin1"
-            redis:
-              host: "192.168.2.168"
-              port: 6379
-            
-            field : "message"
-            match : "\\[.+?\\] INFO [^:]+:(\\d+) (-) .+"
-            extract :
-              - to : "location.line"
-                type : "number"
-              - to : "dash"
-                type : "string"
-        """.trimIndent()
+    fun parseExtractorConfiguration() {
+        val config = parseConfig(yaml)
 
-        val config = Configuration.parse(yaml)
-
-        // Extraction Configuration
         Assertions.assertEquals("message", config.field)
         Assertions.assertEquals("\\[.+?\\] INFO [^:]+:(\\d+) (-) .+", config.match)
         Assertions.assertEquals("location.line", config.extract[0].to)
-        Assertions.assertEquals("number", config.extract[0].type)
         Assertions.assertEquals("dash", config.extract[1].to)
+        Assertions.assertEquals("number", config.extract[0].type)
         Assertions.assertEquals("string", config.extract[1].type)
     }
+}
 
-    @Test
-    fun channelPostfixGetSet() {
-        val yaml = """
-            name: "Plugin1"
-            redis:
-              host: "192.168.2.168"
-              port: 6379
-            
-            field : "message"
-            match : "\\[.+?\\] INFO [^:]+:(\\d+) (-) .+"
-            extract :
-              - to : "location.line"
-                type : "number"
-              - to : "dash"
-                type : "string"
-        """.trimIndent()
+private fun parseConfig(content: String): Configuration {
+    val tempFile = File.createTempFile("config", "yaml")
+    Files.writeString(tempFile.toPath(), content)
 
-        val config = Configuration.parse(yaml)
-
-        val expected = "postfix"
-        config.registrationChannelPostfix = expected
-
-        // Redis Configuration
-        Assertions.assertEquals("postfix", config.registrationChannelPostfix)
-    }
-
-    @Test
-    fun testRedisConfig() {
-        val config = PipelineRedisConfiguration("localhost", 6379)
-
-        Assertions.assertEquals("localhost", config.host)
-        Assertions.assertEquals(6379, config.port)
-    }
+    return readYAMLConfig(tempFile.toPath())
 }
