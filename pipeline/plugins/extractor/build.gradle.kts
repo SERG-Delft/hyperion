395120ad090e5129a7e0b4191f5b1a410edc3126
Ynze ter Horst <ysbrandth@outlook.com>
2020-05-27 15:14:44 +0200

    Remove intelliJ dependencies

--- pipeline/plugins/extractor/build.gradle.kts
+++ pipeline/plugins/extractor/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     application
     jacoco
     kotlin("jvm")
-    id("org.jetbrains.intellij") version "0.4.18"
     id("io.gitlab.arturbosch.detekt").version("1.8.0")
     id("com.github.johnrengelman.shadow").version("5.2.0")
 }
@@ -16,6 +15,11 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.70")
 
     implementation(project(":pipeline:common"))
+
+    // Jackson
+    implementation("com.fasterxml.jackson.core", "jackson-databind", "2.9.4")
+    implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", "2.10.2")
+    implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", "2.9.4")
 }
 
 jacoco {
