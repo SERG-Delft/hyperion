c7f1db519ba72f7bf059ee03ae3c69a35e1d79f7
Ynze ter Horst <ysbrandth@outlook.com>
2020-05-27 10:30:09 +0200

    Adapt configuration tests to new configuration

--- pipeline/plugins/extractor/src/test/kotlin/nl/tudelft/hyperion/pipeline/extractor/configuration/ConfigurationTests.kt
+++ pipeline/plugins/extractor/src/test/kotlin/nl/tudelft/hyperion/pipeline/extractor/configuration/ConfigurationTests.kt
@@ -14,33 +14,53 @@ class ConfigurationTests {
           pluginManager: "1.2.3.4:4567"
           id : "plugin1"
         
-        field : "message"
-        match : "\\[.+?\\] INFO [^:]+:(\\d+) (-) .+"
-        extract :
-          - to : "location.line"
-            type : "number"
-          - to : "dash"
-            type : "string"
+        fields:
+          - field : "message"
+            match : "\\[.+?\\] INFO [^:]+:(\\d+) (-) .+"
+            extract :
+              - to : "location.line"
+                type : "number"
+              - to : "dash"
+                type : "string"
+          - field: "message_2"
+            match: "\\[.+?\\] INFO [^:]+:(\\d+) (-) .+"
+            extract:
+              - to: "location.line_2"
+                type: "number"
+              - to: "dash_2"
+                type: "string"
     """.trimIndent()
 
-    // @Test
-    // fun parsePluginConfiguration() {
-    //     val config = parseConfig(yaml)
-    //
-    //     Assertions.assertEquals("plugin1", config.plugin.id)
-    //     Assertions.assertEquals("1.2.3.4:4567", config.plugin.pluginManager)
-    // }
-    //
-    // @Test
-    // fun parseExtractorConfiguration() {
-    //     val config = parseConfig(yaml)
-    //     Assertions.assertEquals("message", config.field)
-    //     Assertions.assertEquals("""\[.+?\] INFO [^:]+:(\d+) (-) .+""", config.match)
-    //     Assertions.assertEquals("location.line", config.extract[0].to)
-    //     Assertions.assertEquals("dash", config.extract[1].to)
-    //     Assertions.assertEquals(Type.NUMBER, config.extract[0].type)
-    //     Assertions.assertEquals(Type.STRING, config.extract[1].type)
-    // }
+    @Test
+    fun parsePluginConfiguration() {
+        val config = parseConfig(yaml)
+
+        Assertions.assertEquals("plugin1", config.plugin.id)
+        Assertions.assertEquals("1.2.3.4:4567", config.plugin.pluginManager)
+    }
+
+    @Test
+    fun parseExtractorConfiguration() {
+        val config = parseConfig(yaml)
+
+        // field 1
+        val extractableField1 = config.fields[0]
+        Assertions.assertEquals("message", extractableField1.field)
+        Assertions.assertEquals("""\[.+?\] INFO [^:]+:(\d+) (-) .+""", extractableField1.match)
+        Assertions.assertEquals("location.line", extractableField1.extract[0].to)
+        Assertions.assertEquals("dash", extractableField1.extract[1].to)
+        Assertions.assertEquals(Type.NUMBER, extractableField1.extract[0].type)
+        Assertions.assertEquals(Type.STRING, extractableField1.extract[1].type)
+
+        // field 2
+        val extractableField2 = config.fields[1]
+        Assertions.assertEquals("message_2", extractableField2.field)
+        Assertions.assertEquals("""\[.+?\] INFO [^:]+:(\d+) (-) .+""", extractableField2.match)
+        Assertions.assertEquals("location.line_2", extractableField2.extract[0].to)
+        Assertions.assertEquals("dash_2", extractableField2.extract[1].to)
+        Assertions.assertEquals(Type.NUMBER, extractableField2.extract[0].type)
+        Assertions.assertEquals(Type.STRING, extractableField2.extract[1].type)
+    }
 }
 
 private fun parseConfig(content: String): Configuration {
