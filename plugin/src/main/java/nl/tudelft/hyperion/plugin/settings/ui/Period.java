58ef1ee3905f0b834658308ea57a490a389f0c6e
BlackQuests <maartenlips2@gmail.com>
2020-05-28 12:56:04 +0200

    Add javadoc to Period.java and Row.java

--- plugin/src/main/java/nl/tudelft/hyperion/plugin/settings/ui/Period.java
+++ plugin/src/main/java/nl/tudelft/hyperion/plugin/settings/ui/Period.java
@@ -1,5 +1,8 @@
 package nl.tudelft.hyperion.plugin.settings.ui;
 
+/**
+ * Class that represents different periods that we allow to be used as intervals.
+ */
 public enum Period {
     Seconds(1),
     Minutes(60),
@@ -7,9 +10,17 @@ public enum Period {
     Days(86400),
     Weeks(604800);
 
+    /**
+     * Public field that specifies the amount of seconds a period is.
+     * This is used to convert intervals to seconds {@link Row#toSeconds()}.
+     */
     public final int inSeconds;
 
-    private Period(int inSeconds) {
+    /**
+     * Instantiate Period with given amount of seconds.
+     * @param inSeconds this periods value in seconds.
+     */
+    Period(int inSeconds) {
         this.inSeconds = inSeconds;
     }
 }
--- plugin/src/main/java/nl/tudelft/hyperion/plugin/settings/ui/Row.java
+++ plugin/src/main/java/nl/tudelft/hyperion/plugin/settings/ui/Row.java
@@ -2,15 +2,35 @@ package nl.tudelft.hyperion.plugin.settings.ui;
 
 import java.util.Objects;
 
+/**
+ * Class that represents a Row in the IntervalTable {@link IntervalTable}.
+ */
 class Row implements Cloneable {
     private int interval;
     private Period period;
 
+    /**
+     * Instantiate Row with given interval and period {@link Period}.
+     * @param interval interval value (not necessarily in seconds).
+     * @param period the period for the interval.
+     */
     Row(int interval, Period period) {
         this.interval = interval;
         this.period = period;
     }
 
+    /**
+     * Creates a Row using the amount of specified seconds.
+     * The Period for the Row is determined by the greatest possible fit without any seconds left.
+     * Examples:
+     * parse(3600) returns Row(1, Period.Hours)
+     * parse(3660) returns Row(61, Period.Minutes)
+     * parse(60) returns Row(1, Period.Minutes)
+     * parse(61) returns Row(61, Period.Seconds)
+     *
+     * @param inSeconds interval in seconds
+     * @return a Row with given interval, clipped to greatest possible Period.
+     */
     static Row parse(int inSeconds) {
         if (inSeconds % Period.Weeks.inSeconds == 0)
             return new Row(inSeconds / Period.Weeks.inSeconds, Period.Weeks);
@@ -24,20 +44,52 @@ class Row implements Cloneable {
         return new Row(inSeconds, Period.Seconds);
     }
 
+    /**
+     * Returns the column value of the row.
+     * If columnIndex is 0 we return the first column (interval), otherwise we return the second column (period)
+     * @param columnIndex column to retrieve.
+     * @return column relating to given index.
+     */
     Object getColumn(int columnIndex) {
         if (columnIndex == 0) return interval;
         return period;
     }
 
+    /**
+     * Sets the specified column to specified value.
+     * Will do nothing if value is not of correct type.
+     * @param columnIndex column to change.
+     * @param value value to apply.
+     */
     void setColumn(int columnIndex, Object value) {
         if (columnIndex == 0 && value instanceof Integer) interval = ((Integer) value);
         else if (value instanceof Period) period = ((Period) value);
 
     }
 
+    /**
+     * Returns this Row's interval value in seconds
+     * by multiplying it with the Period in seconds {@link Period#inSeconds}
+     *
+     * @return this Row's interval value in seconds.
+     */
     int toSeconds() {
         return interval * period.inSeconds;
     }
+
+    /**
+     * Clones the values of this row to a new one.
+     * @return a new Row with the same values.
+     */
+    public Row clone() {
+        try {
+            return ((Row) super.clone());
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return new Row(interval, period);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -54,9 +106,5 @@ class Row implements Cloneable {
     @Override
     public String toString() {
         return interval + " " + period;
-    }
-
-    public Row clone() {
-        return new Row(interval, period);
     }
 }
