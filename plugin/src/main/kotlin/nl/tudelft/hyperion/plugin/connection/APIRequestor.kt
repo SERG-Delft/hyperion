aca1f98fcc2a2112359dd34f8aa74eebd9c74560
BlackQuests <maartenlips2@gmail.com>
2020-05-28 12:00:47 +0200

    Integrate state address into APIRequestor.kt

--- plugin/src/main/kotlin/nl/tudelft/hyperion/plugin/connection/APIRequestor.kt
+++ plugin/src/main/kotlin/nl/tudelft/hyperion/plugin/connection/APIRequestor.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.intellij.openapi.project.Project
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
 import nl.tudelft.hyperion.plugin.metric.FileMetrics
 import nl.tudelft.hyperion.plugin.settings.HyperionSettings
 
@@ -18,48 +19,13 @@ object APIRequestor {
         mapper.registerModule(module)
     }
 
-    public suspend fun getMetricForFile(filePath: String, project: Project): FileMetrics {
+    public suspend fun getMetricForFile(filePath: String, ideProject: Project): FileMetrics {
         // TODO: Remove hardcoded intervals & project
-        val state = HyperionSettings.getInstance(project).state ?: return FileMetrics(emptyMap())
+        val state = HyperionSettings.getInstance(ideProject).state
         val intervals = state.intervals.joinToString(",")
         val project = "TestProject"
 
-        // val json: String = client.get("/api/v1/metrics?project=$project&file=$filePath&intervals=$intervals")
-        val json = """
-            [{
-                "interval": 60,
-                "versions": {
-                    "6b72b792fc2241a74592fcd11840577508de796d": [{
-                        "line": 11,
-                        "count": 20,
-                        "severity": "INFO"
-                    }, {
-                        "line": 19,
-                        "count": 20,
-                        "severity": "INFO"
-                    }],
-                    "a8c21c597f39502f85090f6abf795e716ef336e2": [{
-                        "line": 11,
-                        "count": 1,
-                        "severity": "DEBUG"
-                    }]
-                }
-            }, {
-                "interval": 120,
-                "versions": {
-                    "6b72b792fc2241a74592fcd11840577508de796d": [{
-                        "line": 11,
-                        "count": 20,
-                        "severity": "INFO"
-                    }],
-                    "a8c21c597f39502f85090f6abf795e716ef336e2": [{
-                        "line": 19,
-                        "count": 1,
-                        "severity": "DEBUG"
-                    }]
-                }
-            }]
-        """.trimIndent()
+        val json: String = client.get("${state.address}?project=$project&file=$filePath&intervals=$intervals")
 
         return FileMetrics.fromMetricsResults(mapper.readValue(json))
     }
