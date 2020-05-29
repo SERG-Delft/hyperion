d68504eb505a0d8ebb06c4fa6740721ccd890601
Thijs Molendijk <info@thijsmolendijk.nl>
2020-05-29 12:17:27 +0200

    feat(pipeline:pathextractor): Rename config field, robustness, support Kt suffix removal

--- pipeline/plugins/pathextractor/src/main/kotlin/nl/tudelft/hyperion/pipeline/pathextractor/Main.kt
+++ pipeline/plugins/pathextractor/src/main/kotlin/nl/tudelft/hyperion/pipeline/pathextractor/Main.kt
@@ -4,9 +4,8 @@ package nl.tudelft.hyperion.pipeline.pathextractor
 
 import nl.tudelft.hyperion.pipeline.runPipelinePlugin
 
-fun main(vararg args: String) {
-    runPipelinePlugin(
-            args.get(0),
-            ::ExtractPathPlugin
-    )
-}
+fun main(vararg args: String) = runPipelinePlugin(
+    args[0],
+    ::ExtractPathPlugin
+)
+
