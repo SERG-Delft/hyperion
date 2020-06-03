0feed88ce4f94bab276294904edab30fce30c465
David Laurentius Arie Moolenaar <david@modare.nl>
2020-06-03 11:41:38 +0200

    Add rate plugin

---
+++ pipeline/plugins/rate/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    application
+    jacoco
+    kotlin("jvm")
+    id("io.gitlab.arturbosch.detekt").version("1.8.0")
+    id("com.github.johnrengelman.shadow").version("5.2.0")
+}
+
+application {
+    mainClassName = "nl.tudelft.hyperion.pipeline.plugins.rate.Main"
+}
+
+dependencies {
+    // Kotlin class reflection
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.70")
+
+    // testing
+    testImplementation("io.mockk", "mockk", "1.10.0")
+
+    // ZeroMQ
+    implementation("org.zeromq", "jeromq", "0.5.2")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8", "1.3.5")
+
+    // Logging
+    implementation("io.github.microutils", "kotlin-logging", "1.7.9")
+    implementation("org.slf4j", "slf4j-simple", "1.7.28")
+
+    // local modules
+    implementation(project(":pipeline:common"))
+}
+
+jacoco {
+    toolVersion = "0.8.5"
+    reportsDir = file("$buildDir/jacoco")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = false
+        html.destination = file("${buildDir}/jacocoHtml")
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                counter = "BRANCH"
+                minimum = "0".toBigDecimal()
+            }
+
+            limit {
+                counter = "LINE"
+                minimum = "0.8".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
+tasks.build {
+    dependsOn(tasks.jacocoTestReport)
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+    destinationDirectory.set(File("./build"))
+}
---
+++ pipeline/plugins/rate/src/test/kotlin/nl/tudelft/hyperion/pipeline/plugins/rate/RatePluginTest.kt
@@ -0,0 +1,47 @@
+package nl.tudelft.hyperion.pipeline.plugins.rate
+
+import kotlinx.coroutines.runBlocking
+import nl.tudelft.hyperion.pipeline.PipelinePluginConfiguration
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class RatePluginTest {
+    private val config = RateConfiguration(PipelinePluginConfiguration("rate", "localhost"))
+
+    @Test
+    fun `process should increase throughput count`() {
+        val plugin = RatePlugin(config)
+
+        runBlocking { plugin.process("message") }
+
+        assertEquals(1, plugin.throughput.get())
+    }
+
+    @Test
+    fun `throughput should be 0 on start`() {
+        val plugin = RatePlugin(config)
+
+        assertEquals(0, plugin.throughput.get())
+    }
+
+    @Test
+    fun `process should return input`() {
+        val plugin = RatePlugin(config)
+
+        val ret = runBlocking { plugin.process("message") }
+
+        assertEquals("message", ret)
+    }
+
+    @Test
+    fun `report should reset count to 0`() {
+        val plugin = RatePlugin(config)
+
+        runBlocking {
+            plugin.process("message")
+            plugin.report()
+        }
+
+        assertEquals(0, plugin.throughput.get())
+    }
+}
