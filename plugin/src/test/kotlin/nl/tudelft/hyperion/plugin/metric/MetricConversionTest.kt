6ff540827b08a342431f87854faa065a82472274
BlackQuests <maartenlips2@gmail.com>
2020-06-03 14:09:01 +0200

    Add test for conversion w/ unresolved line from GitVersionTracker

--- plugin/src/test/kotlin/nl/tudelft/hyperion/plugin/metric/MetricConversionTest.kt
+++ plugin/src/test/kotlin/nl/tudelft/hyperion/plugin/metric/MetricConversionTest.kt
@@ -6,7 +6,6 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkObject
 import nl.tudelft.hyperion.plugin.git.GitLineTracker
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import kotlin.test.assertEquals
@@ -22,11 +21,13 @@ class MetricConversionTest {
     init {
         mockkObject(GitLineTracker)
 
-        every { GitLineTracker.resolveCurrentLine(any(), any(), any(), any()) } returns 0
+
     }
 
     @Test
     fun `Test conversion methods for simple case`() {
+        every { GitLineTracker.resolveCurrentLine(any(), any(), any(), any()) } returns 0
+
         val apiMetricsResults = arrayOf(APIMetricsResult(1, mapOf(
                 "HEAD" to listOf(APIMetric(0, "DEBUG", 20))
         )))
@@ -49,5 +50,20 @@ class MetricConversionTest {
         assertEquals(lineSumsExpected, resolvedFileMetricsActual.lineSums)
         assertEquals(resolvedFileMetricsExpected, resolvedFileMetricsActual)
 
+    }
+
+    @Test
+    fun `Test conversion methods with unresolved line`() {
+        every { GitLineTracker.resolveCurrentLine(any(), any(), any(), any()) } returns null
+
+        val fileMetrics = FileMetrics(mapOf(
+                0 to LineMetrics(mapOf(
+                        1 to listOf(LineIntervalMetric("HEAD", 20))
+                ))
+        ))
+        val resolvedFileMetricsExpected = ResolvedFileMetrics(fileMetrics, emptyMap())
+        val resolvedFileMetricsActual = ResolvedFileMetrics.resolve(fileMetrics, project, file)
+
+        assertEquals(resolvedFileMetricsExpected, resolvedFileMetricsActual)
     }
 }
