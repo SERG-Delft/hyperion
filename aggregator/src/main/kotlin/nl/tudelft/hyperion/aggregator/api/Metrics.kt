21e4d1bba549c27f4a2b8eb6d564ef2eaf74e892
Nick Yu <nickyu42@gmail.com>
2020-06-03 16:17:17 +0200

    feat(aggregator): Add file field in metrics result

--- aggregator/src/main/kotlin/nl/tudelft/hyperion/aggregator/api/Metrics.kt
+++ aggregator/src/main/kotlin/nl/tudelft/hyperion/aggregator/api/Metrics.kt
@@ -29,8 +29,17 @@ data class MetricsResult(
  */
 data class BinnedMetricsResult(
     val startTime: Long,
-    val intervalLength: Int,
-    val versions: Map<String, List<Metric>>
+    val versions: Map<String, List<BaseMetric>>
+)
+
+/**
+ * Represents a base metric which can be extended with additional
+ * fields.
+ */
+sealed class BaseMetric(
+    open val line: Int,
+    open val severity: String,
+    open val count: Int
 )
 
 /**
@@ -40,10 +49,20 @@ data class BinnedMetricsResult(
  * triggered.
  */
 data class Metric(
-    val line: Int,
-    val severity: String,
-    val count: Int
-)
+    override val line: Int,
+    override val severity: String,
+    override val count: Int
+) : BaseMetric(line, severity, count)
+
+/**
+ * Represents a metric with the originating file as an additional field.
+ */
+data class FileMetric(
+    override val line: Int,
+    override val severity: String,
+    override val count: Int,
+    val file: String
+) : BaseMetric(line, severity, count)
 
 /**
  * Computes aggregated metrics from the database for the specified project, file
@@ -62,7 +81,7 @@ fun computeMetrics(
     val endTime = DateTime.now()
 
     transaction {
-        val grouped = computeMetricsQuery(file, project, startTime, endTime)
+        val grouped = computeMetricsQueryFile(file, project, startTime, endTime)
 
         // Convert to expected format.
         MetricsResult(interval, grouped.mapValues { (_, rows) ->
@@ -78,9 +97,18 @@ fun computeMetrics(
 }
 
 /**
- * Compute aggregated on per-period basis for the specified project, file
- * and intervals. This will run async and return a job that can be awaited for the
+ * Compute aggregated on per-period basis for the specified project, file and
+ * intervals. This will run async and return a job that can be awaited for the
  * result of the computation.
+ *
+ * @param configuration configuration of the plugin.
+ * @param project the name of the project to aggregate metrics of.
+ * @param file the name of the file to aggregate metrics of.
+ *  Can optionally be null if querying project wide metrics.
+ * @param relativeTime the relative time from current time to get metrics of.
+ * @param steps the amount of steps to split the relative time in.
+ *
+ * @return map of interval and binned log metrics.
  */
 fun computePeriodicMetrics(
     configuration: Configuration,
@@ -88,7 +116,7 @@ fun computePeriodicMetrics(
     file: String? = null,
     relativeTime: Int,
     steps: Int
-): List<BinnedMetricsResult> {
+): Map<String, Any> {
     // Clamp to values within bounds.
     val clampedTime = min(relativeTime, configuration.aggregationTtl)
     val startTime = DateTime.now().minusSeconds(clampedTime)
@@ -102,14 +130,27 @@ fun computePeriodicMetrics(
     // Create the start time of each interval
     val startTimes = (0 until (clampedTime / interval)).map { startTime.plusSeconds(it * interval) }
 
-    return startTimes.map { start ->
+    val result = mutableMapOf<String, Any>("interval" to interval)
+    result["results"] = startTimes.map { start ->
         transaction {
-            val grouped = computeMetricsQuery(file, project, start, start.plusSeconds(interval))
+            val metrics = if (file == null) {
+                // Retrieve project wide statistics
+                val grouped = computeMetricsQuery(project, start, start.plusSeconds(interval))
 
-            // Convert to expected format.
-            BinnedMetricsResult(
-                start.millis / 1000,
-                interval,
+                grouped.mapValues { (_, rows) ->
+                    rows.map {
+                        FileMetric(
+                            it[AggregationEntries.line],
+                            it[AggregationEntries.severity],
+                            it[AggregationEntries.numTriggers.sum()]!!,
+                            it[AggregationEntries.file]
+                        )
+                    }
+                }
+            } else {
+                // Retrieve statistics of specific file
+                val grouped = computeMetricsQueryFile(file, project, start, start.plusSeconds(interval))
+
                 grouped.mapValues { (_, rows) ->
                     rows.map {
                         Metric(
@@ -118,22 +159,72 @@ fun computePeriodicMetrics(
                             it[AggregationEntries.numTriggers.sum()]!!
                         )
                     }
-                })
+                }
+            }
+
+            // Convert to expected format.
+            BinnedMetricsResult(
+                start.millis / 1000,
+                metrics
+            )
         }
     }
+
+    return result
 }
 
 /**
- * Computes aggregated metrics from the database between the given intervals.
+ * Computes project wide aggregated metrics from the database between the
+ * given intervals.
  *
- * @param file the name of the file to aggregate metrics of.
- *  Can optionally be null if querying project wide metrics.
  * @param project the name of the project to aggregate metrics of.
  * @param startTime starting timestamp of aggregated logs.
  * @param endTime ending timestamp of aggregated logs.
  */
 fun computeMetricsQuery(
-    file: String? = null,
+    project: String,
+    startTime: DateTime,
+    endTime: DateTime
+): Map<String, List<ResultRow>> =
+    // Group on version
+    AggregationEntries
+        // SELECT version, line, severity, file, SUM(num_triggers)
+        .slice(
+            AggregationEntries.version,
+            AggregationEntries.line,
+            AggregationEntries.severity,
+            AggregationEntries.file,
+            AggregationEntries.numTriggers.sum()
+        )
+        // WHERE project = ?
+        .select {
+            (AggregationEntries.project eq project) and
+                (AggregationEntries.timestamp greater startTime) and
+                (AggregationEntries.timestamp less endTime)
+        }
+        // ORDER BY line ASC
+        .orderBy(AggregationEntries.line)
+        // GROUP BY file, version, severity, line
+        .groupBy(
+            AggregationEntries.file,
+            AggregationEntries.version,
+            AggregationEntries.severity,
+            AggregationEntries.line
+        )
+        // group by version string
+        .groupBy { it[AggregationEntries.version] }
+
+/**
+ * Computes aggregated metrics from the database between the given intervals
+ * for the given file.
+ *
+ * @param file the name of the file to aggregate metrics of.
+ * @param project the name of the project to aggregate metrics of.
+ * @param startTime starting timestamp of aggregated logs.
+ * @param endTime ending timestamp of aggregated logs.
+ */
+fun computeMetricsQueryFile(
+    file: String,
     project: String,
     startTime: DateTime,
     endTime: DateTime
@@ -149,17 +240,10 @@ fun computeMetricsQuery(
         )
         // WHERE file = ? AND project = ?
         .select {
-            val base =
-                (AggregationEntries.project eq project) and
-                    (AggregationEntries.timestamp greater startTime) and
-                    (AggregationEntries.timestamp less endTime)
-
-            // Add file check if not querying project wide statistics
-            if (file != null) {
-                return@select base and (AggregationEntries.file eq file)
-            }
-
-            base
+            (AggregationEntries.project eq project) and
+                (AggregationEntries.timestamp greater startTime) and
+                (AggregationEntries.timestamp less endTime) and
+                (AggregationEntries.file eq file)
         }
         // ORDER BY line ASC
         .orderBy(AggregationEntries.line)
