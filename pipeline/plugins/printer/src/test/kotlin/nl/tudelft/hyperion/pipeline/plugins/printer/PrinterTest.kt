108fe2c9b8398231922d2e7bc857142b172d8307
Thijs Molendijk <info@thijsmolendijk.nl>
2020-06-05 16:31:50 +0200

    refactor: Update tests for new interface

--- pipeline/plugins/printer/src/test/kotlin/nl/tudelft/hyperion/pipeline/plugins/printer/PrinterTest.kt
+++ pipeline/plugins/printer/src/test/kotlin/nl/tudelft/hyperion/pipeline/plugins/printer/PrinterTest.kt
@@ -9,9 +9,9 @@ class PrinterTest {
     @Test
     fun `Debug plugin should return input`() {
         val plugin = PrinterPlugin(PipelinePluginConfiguration("printer", "host:3000"))
-        runBlocking{
-            val output = plugin.process("input")
-            Assertions.assertEquals(output, "input")
+
+        runBlocking {
+            plugin.onMessageReceived("input")
         }
     }
 }
