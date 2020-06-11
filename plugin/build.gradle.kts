3f21dc412807aa397315a5f8b993600ebe8987ae
BlackQuests <maartenlips2@gmail.com>
2020-06-11 15:26:28 +0200

    Fix building of Intellij Plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -8,7 +8,12 @@ setupJacocoPlugin(branchCoverage = 0.3, lineCoverage = 0.4)
 
 intellij {
     setPlugins("git4idea")
+    version = "2020.1"
 }
+tasks.patchPluginXml {
+    sinceBuild("201.4163")
+}
+
 
 dependencies {
     // To establish a connection and make a get request to the API.
