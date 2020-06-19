0280b9e40be6b38454433c9dbfa16d62828237d8
BlackQuests <maartenlips2@gmail.com>
2020-06-19 11:18:42 +0200

    Convert HyperionNotifier to object

--- plugin/src/main/kotlin/nl/tudelft/hyperion/plugin/util/HyperionNotifier.kt
+++ plugin/src/main/kotlin/nl/tudelft/hyperion/plugin/util/HyperionNotifier.kt
@@ -10,35 +10,33 @@ import nl.tudelft.hyperion.plugin.visualization.GUTTER_ICON
  * Class that handles notifications for the Hyperion plugin.
  * These notifications are project-bound and show in the event log.
  */
-class HyperionNotifier {
-    companion object {
-        private val NOTIFICATION_GROUP = NotificationGroup(
-            "com.github.serg-delft.hyperion",
-            NotificationDisplayType.BALLOON,
-            true,
-            "nl.tudelft.hyperion.plugin.visualization.actions.GraphActionGroup",
-            GUTTER_ICON,
-            "Hyperion"
-        )
+object HyperionNotifier {
+    private val NOTIFICATION_GROUP = NotificationGroup(
+        "com.github.serg-delft.hyperion",
+        NotificationDisplayType.BALLOON,
+        true,
+        "nl.tudelft.hyperion.plugin.visualization.actions.GraphActionGroup",
+        GUTTER_ICON,
+        "Hyperion"
+    )
 
-        /**
-         * Sends an error notification to the given project with given content.
-         * @param project Project to send the notification in.
-         * @param content The content of the notification.
-         */
-        fun error(project: Project, content: String) {
-            notify(project, content, NotificationType.ERROR)
-        }
-        // Other types of notifications can go here but for now we only need error.
+    /**
+     * Sends an error notification to the given project with given content.
+     * @param project Project to send the notification in.
+     * @param content The content of the notification.
+     */
+    fun error(project: Project, content: String) {
+        notify(project, content, NotificationType.ERROR)
+    }
+    // Other types of notifications can go here but for now we only need error.
 
-        /**
-         * Creates an event log notification in the given project with given content and type.
-         * @param project Project to send the notification in.
-         * @param content The content of the notification.
-         * @param type The type of notification to send.
-         */
-        private fun notify(project: Project, content: String, type: NotificationType) {
-            NOTIFICATION_GROUP.createNotification(content, type).notify(project)
-        }
+    /**
+     * Creates an event log notification in the given project with given content and type.
+     * @param project Project to send the notification in.
+     * @param content The content of the notification.
+     * @param type The type of notification to send.
+     */
+    private fun notify(project: Project, content: String, type: NotificationType) {
+        NOTIFICATION_GROUP.createNotification(content, type).notify(project)
     }
 }
