82ed5d00b7e9e093ecc3a3ad2b2b043678458c85
David Laurentius Arie Moolenaar <david@modare.nl>
2020-06-19 09:05:17 +0200

    Remove docker tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.71"
-    id("com.bmuschko.docker-remote-api") version "6.4.0"
 }
 
 group = "nl.tudelft.hyperion"
@@ -73,80 +72,12 @@ subprojects {
     }
 }
 
-// val releaseArtifacts = listOf(":pluginmanager:", ":datasource:plugins:elasticsearch:", ":aggregator:",
-//                               ":pipeline:plugins:adder:", ":pipeline:plugins:extractor:",
-//                               ":pipeline:plugins:loadbalancer:", ":pipeline:plugins:pathextractor:",
-//                               ":pipeline:plugins:printer:", ":pipeline:plugins:rate:",
-//                               ":pipeline:plugins:reader:", ":pipeline:plugins:renamer:",
-//                               ":pipeline:plugins:stresser:", ":pipeline:plugins:versiontracker:")
-
-val releaseArtifacts = listOf(":pipeline:plugins:adder:")
-
-tasks.register<DockerBuildImage>("docker-image") {
-    group = "docker"
-    description = "Create docker image"
-    doFirst {
-        println("building image")
-    }
-}
-
-tasks.register<DockerPushImage>("docker-push") {
-    group = "docker"
-    description = "Pushed a docker image to docker hub"
-    doFirst {
-        println("pushing image")
-    }
-}
-
-configure(subprojects.filter { releaseArtifacts.contains<String>(it.path + ":")}) {
-    version = (if (project.properties["version"] != "unspecified") { project.properties["version"] } else { "0.1.0" })!!
-    val dockerId = "daveter9"
-    // format the artifact name in the necessary formats
-    val artifact = project.path + ":"
-    val artifactParts = artifact.split(":").drop(1).dropLast(1)
-    val artifactName = "hyperion-${artifactParts.joinToString("-", "", "", limit = -1)}:$version"
-
-    val imageName = "$dockerId/$artifactName"
-
-    tasks.register<DockerBuildImage>("docker-image") {
-        dependsOn(artifact + "shadowJar")
-
-        docker {
-            url.set(project.properties["dockerHost"].toString())
-            //dockerFile.set(dockerfileArtifactPath)
-            inputDir.set(File("."))
-            imageId.set(artifactName)
-            images.add(imageName)
-            registryCredentials {
-                username.set(project.properties["dockerUsername"].toString())
-                password.set(project.properties["dockerPassword"].toString())
-                email.set(project.properties["dockerEmail"].toString())
-            }
-        }
-    }
-
-    tasks.register<DockerPushImage>("docker-push") {
-        dependsOn(artifact + "docker-image")
-        //println("docker image $project")
-        docker {
-            url.set(project.properties["dockerHost"].toString())
-            registryCredentials {
-                username.set(project.properties["dockerUsername"].toString())
-                password.set(project.properties["dockerPassword"].toString())
-                email.set(project.properties["dockerEmail"].toString())
-            }
-            images.add(imageName)
-        }
-    }
-}
-
-tasks.register<DefaultTask>("docker-release") {
-    group = "docker"
-    description = "Release all artifacts as docker image on docker hub"
-    for (artifact in releaseArtifacts) {
-        dependsOn(artifact + "docker-push")
-    }
-}
+val releaseArtifacts = listOf(":pluginmanager:", ":datasource:plugins:elasticsearch:", ":aggregator:",
+                              ":pipeline:plugins:adder:", ":pipeline:plugins:extractor:",
+                              ":pipeline:plugins:loadbalancer:", ":pipeline:plugins:pathextractor:",
+                              ":pipeline:plugins:printer:", ":pipeline:plugins:rate:",
+                              ":pipeline:plugins:reader:", ":pipeline:plugins:renamer:",
+                              ":pipeline:plugins:stresser:", ":pipeline:plugins:versiontracker:")
 
 tasks.register<DefaultTask>("build-artifacts-release") {
     group = "build"
